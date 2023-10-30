a86ebedc9526cf07178abced49a13871428c280d
a123new <137861561+a123new@users.noreply.github.com>
2023-10-30 16:52:41 +0530

    Add files via upload

---
+++ Hands-on/ParentChildDemo/src/main/java/com/hiber1/demo/Child.java
@@ -0,0 +1,33 @@
+package com.hiber1.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+@Entity
+public class Child
+{
+	@Id
+	private int cId;
+	private String cName;
+	@ManyToOne
+	private Parent parent;
+	
+	public int getcId() {
+		return cId;
+	}
+	public void setcId(int cId) {
+		this.cId = cId;
+	}
+	public String getcName() {
+		return cName;
+	}
+	public void setcName(String cName) {
+		this.cName = cName;
+	}
+	public Parent getParent() {
+		return parent;
+	}
+	public void setParent(Parent parent) {
+		this.parent = parent;
+	}
+}
---
+++ Hands-on/ParentChildDemo/src/main/java/com/hiber1/demo/Parent.java
@@ -0,0 +1,45 @@
+package com.hiber1.demo;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+@Entity
+public class Parent {
+
+	@Id
+	private int pId;
+	@Column
+	private String pName;
+	@OneToMany
+	private Set<Child> child;
+	
+	public int getpId() {
+		return pId;
+	}
+	public void setpId(int pId) {
+		this.pId = pId;
+	}
+	public String getpName() {
+		return pName;
+	}
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+	public Set<Child> getChild() {
+		return child;
+	}
+	public void setChild(Set<Child> child) {
+		this.child = child;
+	}
+	@Override
+	public String toString() {
+		return "Parent [pId=" + pId + ", pName=" + pName + ", child=" + child + "]";
+	}
+	
+	
+	
+	
+}
